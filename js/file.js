f4be36cd7ea10acd6f9cf6dd77ec1ea9bb0d8a45
userlg <guipeluis90@gmail.com>
2020-10-05 18:53:39 -0400

    Funcion factorial creada y se declara su llamada en el archivo por consola

--- js/file.js
+++ js/file.js
@@ -10,6 +10,16 @@ function sumar(num1, num2){
     return num1 + num2;
 }
 
+function factorial(num1){
+    var resultado = 1;
+    
+    while(num1>1){
+        resultado = resultado * num1;
+        num1= num1-1;
+    }
+    return resultado;
+}
+
 
 alert(77);
 
@@ -21,6 +31,7 @@ var texto = prompt(m1);  //Funcion crea una pequeña ventana de texto para intro
 
 document.write("<h2>Bienvenido "+texto+"</h2>");
 console.log(sumar(15,5));
+console.log(factorial(4));
 
 
 console.log('Bienvenido',+texto);
