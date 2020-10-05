3c8810c2a9d7841e535f42261613ec156569a453
userlg <guipeluis90@gmail.com>
2020-10-05 18:21:05 -0400

    Segundo Cambio

--- js/file.js
+++ js/file.js
@@ -1,4 +1,21 @@
-alert(77);
-document.write('Hola prueba de javascript');
 
-console.log('77');
+function nuevoContenido() {
+ // alert("carga el contenido nuevo");
+  document.open();
+  document.write("<h1>Aplicacion Sencilla de muestra JavaScript</h1>");
+  document.close();
+}
+
+function sumar(num1, num2){
+    return num1 + num2;
+}
+
+
+alert(77);
+
+nuevoContenido();
+
+console.log(sumar(15,5));
+
+
+console.log('Bienvenido');
